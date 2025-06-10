3e2088b58627c0da6952d2ea2813c9ca25d8cebe
xhengis miftari <xhengismiftari@gmail.com>
2025-06-10 15:36:16 +0200

    new stuff

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  has_one_attached :avatar
   has_many :projects
   has_many :clients, through: :projects
   has_many :invoices, through: :projects
