5676ad1ddaf1736a0991e72bcf732ad0b564e1d2
sartoscht loock <sartoscht.loock@gmail.com>
2025-06-10 11:59:32 +0200

    added the view to the new page

--- app/models/client.rb
+++ app/models/client.rb
@@ -3,7 +3,7 @@ class Client < ApplicationRecord
   has_many :invoices, through: :projects
   has_many :project_dates, through: :projects
 
-  validates :first_name, :last_name, :email, :phone_number, :date_of_birth presence: true
+  validates :first_name, :last_name, :email, :phone_number, :date_of_birth, presence: true
   validates :email,
     format: { with: URI::MailTo::EMAIL_REGEXP, message: "must be a valid email" },
     uniqueness: { case_sensitive: false }
