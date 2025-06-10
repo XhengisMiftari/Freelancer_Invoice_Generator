ae70e30d6389ca44b4cba45b7b196ccd34ee6ad5
Aleksandar Husagic <aleksandar.husagic.ns@gmail.com>
2025-06-10 11:51:51 +0200

    changes

--- app/controllers/project_dates_controller.rb
+++ app/controllers/project_dates_controller.rb
@@ -1,9 +1,2 @@
 class ProjectDatesController < ApplicationController
-  def index
-    @projects = current_user.projects.includes(:client)
-    @projects = Project.new
-  end
-
-  def create
-  end
 end
--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,2 +1,10 @@
 class ProjectsController < ApplicationController
+  def index
+   # @projects = current_user.projects.includes(:client)
+    @projects = Project.all
+    @project = Project.new
+  end
+
+  def create
+  end
 end
