a143799230d613500869031f54339ceb83ba84a3
Aleksandar Husagic <aleksandar.husagic.ns@gmail.com>
2025-06-10 12:11:51 +0200

    changes

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -6,5 +6,19 @@ class ProjectsController < ApplicationController
   end
 
   def create
+    @project = Project.new(project_params)
+    @project.user = current_user
+
+    if @project.save
+      redirect_to projects_path, notice: 'Project was created. Congratulations!'
+    else
+      render :index
+    end
+  end
+
+  private
+
+ def project_params
+    params.require(:project).permit(:name, :price, :status, :client)
   end
 end
