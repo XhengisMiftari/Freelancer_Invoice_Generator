e808e12c73ff15ebd912ac8be1c2db345a9df22b
xhengis miftari <xhengismiftari@gmail.com>
2025-06-10 10:59:47 +0200

    Seeding done

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_06_09_143931) do
+ActiveRecord::Schema[7.1].define(version: 2025_06_10_084431) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,7 +19,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_06_09_143931) do
     t.string "last_name"
     t.date "date_of_birth"
     t.string "address"
-    t.integer "phone_number"
+    t.string "phone_number"
     t.string "company_name"
     t.string "email"
     t.datetime "created_at", null: false
@@ -68,7 +68,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_06_09_143931) do
     t.string "last_name"
     t.date "date_of_birth"
     t.string "address"
-    t.integer "phone_number"
+    t.string "phone_number"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
