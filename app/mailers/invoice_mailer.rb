6124653543a6eec1ed9898c5fdfdae5411edf2e6
xhengis miftari <xhengismiftari@gmail.com>
2025-06-11 12:10:08 +0200

    some new buttons for invocing

---
+++ app/mailers/invoice_mailer.rb
@@ -0,0 +1,10 @@
+class InvoiceMailer < ApplicationMailer
+  def send_invoice
+    @invoice = params[:invoice]
+    @project = @invoice.project
+    mail(
+      to: @project.client.email, # Make sure client has an email
+      subject: "Your Invoice from #{ @project.name }"
+    )
+  end
+end
