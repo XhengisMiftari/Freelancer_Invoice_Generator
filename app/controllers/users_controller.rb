830793b5c90d2e7b5cdfa5a7e60457c2e8de3d89
xhengis miftari <xhengismiftari@gmail.com>
2025-06-13 10:23:51 +0200

    esignature

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,16 @@
 class UsersController < ApplicationController
+  def update
+
+  if @user.update(user_params)
+    redirect_to edit_user_path(@user), notice: "Signature uploaded."
+  else
+    render :edit
+  end
+end
+
+private
+
+  def user_params
+    params.require(:user).permit(:signature)
+  end
 end
