d78afb40d361be5f68d155770bb9efd2ffcdbc19
sartoscht loock <sartoscht.loock@gmail.com>
2025-06-16 17:31:35 +0200

    styled all forms for now and removed the notices/alerts

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,4 +2,6 @@ class Project < ApplicationRecord
   belongs_to :client
   has_many :invoices, dependent: :destroy
   has_one :project_date, dependent: :destroy
+
+  validates :name, :price, presence: :true
 end
