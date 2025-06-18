f3cf4ad2f7d4ad6518a27d36e7ff32c873ec7ac0
xhengis miftari <xhengismiftari@gmail.com>
2025-06-18 09:25:48 +0200

    new

--- app/mailers/invoice_mailer.rb
+++ app/mailers/invoice_mailer.rb
@@ -5,7 +5,7 @@ class InvoiceMailer < ApplicationMailer
     @stripe_url = params[:stripe_url]
     mail(
       to: @project.client.email, # Make sure client has an email
-      subject: "Your Invoice from #{ @project.name }",
+      subject: "Your Invoice for #{ @project.name }",
       body: "Click this link to pay #{@stripe_url}"
     )
   end
